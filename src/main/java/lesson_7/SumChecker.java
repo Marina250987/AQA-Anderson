c15d2eef5a30cc9723594dbf86db0bfa98beefde
Maryna Amelchenka <marinaamelchenko579@gmail.com>
2025-06-30 21:13:06 +0300

    Fix: change logic for SumCheker

--- src/main/java/lesson_7/SumChecker.java
+++ src/main/java/lesson_7/SumChecker.java
@@ -6,20 +6,24 @@ public class SumChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter 1st number:");
-        int a = scanner.nextInt();
+        System.out.print("Enter 1st number (x1): ");
+        int x1 = scanner.nextInt();
 
-        System.out.println("Enter 2nd number:");
-        int b = scanner.nextInt();
+        System.out.print("Enter 2nd number (x2): ");
+        int x2 = scanner.nextInt();
 
-        int sum = a + b;
-
-        if (sum >= 10 && sum <= 20) {
-            System.out.println("True");
-        } else {
-            System.out.println("False");
-        }
+        boolean result = within10and20(x1, x2);
+        System.out.println("The sum is between 10 and 20: " + result);
 
         scanner.close();
+    }
+
+    public static boolean within10and20(int x1, int x2) {
+        int sum = x1 + x2;
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        } else {
+            return false; }
+
     }
 }
